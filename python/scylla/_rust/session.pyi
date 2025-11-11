7a7c78904f225d0f4d187f4bbdf7263f8de2f918
Patrycja Ziemkiewicz <patrycjaziemkiewicz@Patrycjas-MacBook-Air.local>
2025-11-11 14:34:21 +0100

    Add method to Session to enable executing statements with serialization

--- python/scylla/_rust/session.pyi
+++ python/scylla/_rust/session.pyi
@@ -1,5 +1,16 @@
+from typing import Any
+
+from .statements import PyPreparedStatement
+from .writers import SerializationBuffer
 class Session:
     async def execute(self, request: str) -> RequestResult: ...
+    async def prepare(self, request: str) -> PyPreparedStatement: ...
+    async def execute_unpaged_python(
+            self, prepared: PyPreparedStatement, values: list[Any]
+    ) -> RequestResult: ...
+    async def _execute_raw_bytes(
+            self, prepared: PyPreparedStatement, buffer: SerializationBuffer
+    ) -> RequestResult: ...
 
 class RequestResult:
     def __str__(self) -> str: ...
--- python/scylla/session.py
+++ python/scylla/session.py
@@ -1,1 +1,27 @@
 from ._rust.session import *
+from ._rust.session import RequestResult, Session
+from ._rust.statements import PyPreparedStatement
+from ._rust.session import *
+from ._rust.writers import SerializationBuffer
+from typing import Any
+
+from .serializer import SerializedValues
+
+
+async def execute_unpaged_python(
+        self: Session, prepared: PyPreparedStatement, values: list[Any]
+) -> RequestResult:
+
+    try:
+        serialized_values = SerializedValues(prepared)
+        serialized_values.add_row(values)
+
+        data = serialized_values.get_content()
+
+        return await self._execute_raw_bytes(prepared, data)
+
+    except Exception as e:
+        raise RuntimeError(f"Failed to serialize values: {e}") from e
+
+
+Session.execute_unpaged_python= execute_unpaged_python
