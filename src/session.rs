eff763f56ab4dd7eb057031e333355e40ee6d8e3
Jakub Graczyk <j.graczyk4@student.uw.edu.pl>
2025-11-29 17:09:29 +0100

    update: session statement handling

--- src/session.rs
+++ src/session.rs
@@ -2,11 +2,13 @@ use std::fmt::Write;
 use std::sync::Arc;
 
 use pyo3::exceptions::PyRuntimeError;
+use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::types::PyString;
 use scylla::value::Row;
 
 use crate::RUNTIME;
+use crate::statement::PreparedStatement;
 
 #[pyclass]
 pub(crate) struct Session {
@@ -15,22 +17,66 @@ pub(crate) struct Session {
 
 #[pymethods]
 impl Session {
-    async fn execute(&self, request: Py<PyString>) -> PyResult<RequestResult> {
-        let request_string = Python::with_gil(|py| request.to_str(py))?.to_string();
+    async fn execute(&self, request: PyObject) -> PyResult<RequestResult> {
+        if let Ok(prepared) = Python::with_gil(|py| {
+            let scylla_prepared = request.extract::<Py<PreparedStatement>>(py)?;
+            Ok::<Py<PreparedStatement>, PyErr>(scylla_prepared)
+        }) {
+            let result = self
+                .session_spawn_on_runtime(async move |s| {
+                    s.execute_unpaged(&prepared.get()._inner, &[])
+                        .await
+                        .map_err(|e| {
+                            PyRuntimeError::new_err(format!("Failed execute_unpaged: {}", e))
+                        })
+                })
+                .await?; // Propagate error form closure
+            return Ok(RequestResult { inner: result });
+        }
+
+        if let Ok(text) = Python::with_gil(|py| {
+            let text = request.extract::<Py<PyString>>(py)?;
+            Ok::<String, PyErr>(text.to_string())
+        }) {
+            let result = self
+                .session_spawn_on_runtime(async move |s| {
+                    s.query_unpaged(text, &[]).await.map_err(|e| {
+                        PyRuntimeError::new_err(format!("Failed query_unpaged: {}", e))
+                    })
+                })
+                .await?; // Propagate error form closure
+            return Ok(RequestResult { inner: result });
+        }
+        Err(PyErr::new::<PyTypeError, _>("Invalid request type"))
+    }
+
+    async fn prepare(&self, statement: String) -> PyResult<PreparedStatement> {
+        let session_clone = std::sync::Arc::clone(&self._inner);
+        match session_clone.prepare(statement).await {
+            Ok(prepared) => Ok(PreparedStatement { _inner: prepared }),
+            Err(e) => Err(PyErr::new::<PyRuntimeError, _>(format!(
+                "Failed to prepare statement: {}",
+                e
+            ))),
+        }
+    }
+}
+
+impl Session {
+    async fn session_spawn_on_runtime<F, Fut, R>(&self, f: F) -> PyResult<R>
+    where
+        // closure: takes Arc<scylla::client::session::Session> and returns a future
+        F: FnOnce(Arc<scylla::client::session::Session>) -> Fut + Send + 'static,
+        // for spawn we need Send + 'static
+        Fut: Future<Output = PyResult<R>> + Send + 'static,
+        R: Send + 'static,
+    {
         let session_clone = Arc::clone(&self._inner);
 
-        let result = RUNTIME
-            .spawn(async move {
-                session_clone
-                    .query_unpaged(request_string, &[])
-                    .await
-                    .map_err(|e| {
-                        PyRuntimeError::new_err(format!("Failed to deserialize metadata: {}", e))
-                    })
-            })
+        RUNTIME
+            .spawn(async move { f(session_clone).await })
             .await
-            .expect("Driver should not panic")?;
-        Ok(RequestResult { inner: result })
+            .expect("Runtime failed to spawn task") // It's okay to panic here
     }
 }
 
