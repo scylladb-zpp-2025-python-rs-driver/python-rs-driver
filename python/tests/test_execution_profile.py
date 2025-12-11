bdd23d10120153b4801e76d256bb0ea920193238
Jakub Graczyk <j.graczyk4@student.uw.edu.pl>
2025-12-11 20:08:48 +0100

    add: test for creating session with profile

--- python/tests/test_execution_profile.py
+++ python/tests/test_execution_profile.py
@@ -1,6 +1,7 @@
 import pytest
 from scylla.enums import Consistency, SerialConsistency
 from scylla.execution_profile import ExecutionProfile
+from scylla.session_builder import SessionBuilder
 
 
 def test_execution_profile_builder():
@@ -68,3 +69,15 @@ def test_execution_profile_timeout():
     assert isinstance(profile, ExecutionProfile)
     actual_timeout = profile.get_request_timeout()
     assert actual_timeout == expected_timeout
+
+
+@pytest.mark.asyncio
+@pytest.mark.requires_db
+async def test_create_session_with_profile():
+    expected_timeout = 10.5
+    expected_consistency = Consistency("All")
+    profile = ExecutionProfile(timeout=expected_timeout, consistency=expected_consistency)
+    builder = SessionBuilder(["127.0.0.2"], 9042, execution_profile=profile)
+    session = await builder.connect()
+    result = await session.execute("SELECT * FROM system.local")
+    print(result)
