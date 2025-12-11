900aac3edaf32effa217d2725df3985ca2731bfe
Jakub Graczyk <j.graczyk4@student.uw.edu.pl>
2025-12-11 20:08:48 +0100

    add: tests for creating session with profile

--- python/tests/test_execution_profile.py
+++ python/tests/test_execution_profile.py
@@ -1,6 +1,7 @@
 import pytest
 from scylla.enums import Consistency, SerialConsistency
 from scylla.execution_profile import ExecutionProfile
+from scylla.session_builder import SessionBuilder
 
 
 def test_execution_profile_builder():
@@ -56,3 +57,26 @@ def test_execution_profile_timeout():
     assert isinstance(profile, ExecutionProfile)
     actual_timeout = profile.get_request_timeout()
     assert actual_timeout == expected_timeout
+
+
+@pytest.mark.asyncio
+@pytest.mark.requires_db
+async def test_create_session_with_profile():
+    expected_timeout = 10.5
+    expected_consistency = Consistency.All
+    profile = ExecutionProfile(timeout=expected_timeout, consistency=expected_consistency)
+    builder = SessionBuilder(["127.0.0.2"], 9042, execution_profile=profile)
+    session = await builder.connect()
+    result = await session.execute("SELECT * FROM system.local")
+    print(result)
+
+
+@pytest.mark.asyncio
+@pytest.mark.requires_db
+async def test_invalid_consistency_for_query():
+    profile = ExecutionProfile(consistency=Consistency.Three)
+    builder = SessionBuilder(["127.0.0.2"], 9042, execution_profile=profile)
+    session = await builder.connect()
+    with pytest.raises(RuntimeError) as exc_info:
+        _ = await session.execute("SELECT * FROM system.local")
+        assert "Not enough nodes are alive to satisfy required consistency level" in str(exc_info.value)
