a88788bd688b37aad92f09e5018de96f7f65e4aa
Jakub Graczyk <j.graczyk4@student.uw.edu.pl>
2025-12-11 20:08:18 +0100

    add: execution profile to session builder

--- python/scylla/_rust/session_builder.pyi
+++ python/scylla/_rust/session_builder.pyi
@@ -1,7 +1,10 @@
 from collections.abc import Sequence
 
+from .execution_profile import ExecutionProfile
 from .session import Session
 
 class SessionBuilder:
-    def __init__(self, contact_points: Sequence[str], port: int) -> None: ...
+    def __init__(
+        self, contact_points: Sequence[str], port: int, execution_profile: ExecutionProfile | None = None
+    ) -> None: ...
     async def connect(self) -> Session: ...
