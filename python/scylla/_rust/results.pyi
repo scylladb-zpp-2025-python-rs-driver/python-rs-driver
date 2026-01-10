8209b72e4edee4effa48089525a8a11cfb7881e9
Patrycja Ziemkiewicz <patrycja.z.ziemkiewicz@gmail.com>
2026-01-10 22:22:50 +0100

    Add docstrings to .pyi files

--- python/scylla/_rust/results.pyi
+++ python/scylla/_rust/results.pyi
@@ -1,7 +1,7 @@
 import ipaddress
 from datetime import date, datetime, time
 from decimal import Decimal
-from typing import Any, Dict, List, Set, Tuple, Union
+from typing import Dict, List, Union, Tuple, Any, Set, Optional
 from uuid import UUID
 
 from dateutil.relativedelta import relativedelta
@@ -83,22 +83,65 @@ CqlValue = Union[
 ]
 
 class ColumnIterator:
+    """
+    Iterator over columns of a single row.
+
+    Yields Column objects representing individual column values
+    in the current row.
+    """
     def __iter__(self) -> ColumnIterator: ...
     def __next__(self) -> Column: ...
 
 class RowFactory:
+    """
+    Factory used to construct a row object from a column iterator.
+
+    Allows custom row representations (e.g. dicts, dataclasses).
+    """
+
     def __init__(self) -> None: ...
-    def build(self, column_iterator: ColumnIterator) -> Dict[str, CqlValue]: ...
+
+    def build(self, column_iterator: ColumnIterator) -> Dict[str, CqlValue]:
+        """
+        Build a row object from the provided column iterator.
+        """
+        ...
 
 class Column:
+    """
+    Represents a single column in a result row.
+    """
     @property
-    def column_name(self) -> str: ...
+    def column_name(self) -> str:
+        """Name of the column."""
+        ...
+
     @property
-    def value(self) -> CqlValue: ...
+    def value(self) -> CqlValue:
+        """Deserialized value of the column."""
+        ...
 
 class RowsIterator:
-    def __next__(self) -> Any: ...
+    """
+    Iterator over result rows.
+
+    Each iteration yields a single row object, by default a dictionary
+    mapping column names to values.
+    """
+
     def __iter__(self) -> RowsIterator: ...
+    def __next__(self) -> Any: ...
 
 class RequestResult:
-    def iter_rows(self, factory: RowFactory | None = None) -> RowsIterator: ...
+    """
+    Result of a query execution.
+    """
+
+    def iter_rows(self, factory: Optional[RowFactory] = None) -> RowsIterator:
+        """
+        Return an iterator over result rows.
+
+        An optional RowFactory can be provided to customize how rows
+        are constructed.
+        """
+        ...
