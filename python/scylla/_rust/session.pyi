1da0ed2461969f1b563614f08e0a2b37917d5653
paulinaczajkowska <paulina.czajkowska04@gmail.com>
2026-01-23 14:14:34 +0100

    Implement Rust-side serialization

--- python/scylla/_rust/session.pyi
+++ python/scylla/_rust/session.pyi
@@ -1,6 +1,11 @@
 from .results import RequestResult
 from .statement import PreparedStatement, Statement
+from .types import CqlValueList
 
 class Session:
-    async def execute(self, request: PreparedStatement | Statement | str) -> RequestResult: ...
+    async def execute(
+        self,
+        request: PreparedStatement | Statement | str,
+        values: CqlValueList | None = None,
+    ) -> RequestResult: ...
     async def prepare(self, statement: Statement | str) -> PreparedStatement: ...
--- python/scylla/_rust/types.pyi
+++ python/scylla/_rust/types.pyi
@@ -1,3 +1,10 @@
+from typing import Sequence, AbstractSet, TypeAlias, Mapping
+from .results import CqlNative
+import ipaddress
+from datetime import date, datetime, time
+from decimal import Decimal
+from uuid import UUID
+
 class UnsetType:
     """
     Type of the Unset singleton.
@@ -5,3 +12,27 @@ class UnsetType:
 
     def __repr__(self) -> str: ...
     def __str__(self) -> str: ...
+
+CqlCollection: TypeAlias = (
+    # CQL: List, Tuple, Vector
+    Sequence["CqlValue"]
+    # CQL: Set
+    | AbstractSet["CqlValue"]
+    # CQL: Map, UserDefinedType (UDT)
+    | Mapping[int, "CqlValue"]
+    | Mapping[float, "CqlValue"]
+    | Mapping[str, "CqlValue"]
+    | Mapping[bool, "CqlValue"]
+    | Mapping[bytes, "CqlValue"]
+    | Mapping[Decimal, "CqlValue"]
+    | Mapping[UUID, "CqlValue"]
+    | Mapping[ipaddress.IPv4Address, "CqlValue"]
+    | Mapping[ipaddress.IPv6Address, "CqlValue"]
+    | Mapping[date, "CqlValue"]
+    | Mapping[datetime, "CqlValue"]
+    | Mapping[time, "CqlValue"]
+)
+
+CqlValue = CqlCollection | CqlNative
+
+CqlValueList: TypeAlias = Sequence[CqlValue] | Mapping[str, CqlValue]
