ac157b9c583ac18f3e8b8c6ec8cea4f7379010de
Patrycja Ziemkiewicz <patrycja.z.ziemkiewicz@gmail.com>
2026-04-20 13:20:42 +0200

    Add schema_agreement_timeout to SessionBuilder

--- src/session_builder.rs
+++ src/session_builder.rs
@@ -355,6 +355,18 @@ impl SessionBuilder {
         }
         slf
     }
+
+    fn schema_agreement_timeout<'py>(
+        slf: PyRef<'py, Self>,
+        py: Python<'py>,
+        timeout: PyDuration,
+    ) -> PyRef<'py, Self> {
+        {
+            let mut inner = slf.inner.lock_py_attached(py).unwrap();
+            inner.config.schema_agreement_timeout = timeout.0;
+        }
+        slf
+    }
     async fn connect(&self) -> Result<PySession, DriverSessionConnectionError> {
         let config = Python::attach(|py| {
             let inner = self.inner.lock_py_attached(py).unwrap();
