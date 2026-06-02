86c867be4a042db6f60bafb39bb38edbf97dacbf
Paulina Czajkowska <paulina.czajkowska04@gmail.com>
2026-06-02 22:15:13 +0200

    add: `PyRetryDecision`

--- python/scylla/policies/retry_policy.py
+++ python/scylla/policies/retry_policy.py
@@ -1,11 +1,13 @@
 from .._rust.policies.retry_policy import (  # pyright: ignore[reportMissingModuleSource]
     CqlResponseKind,
     OperationType,
+    RetryDecision,
     WriteType,
 )
 
 __all__ = [
     "CqlResponseKind",
     "OperationType",
+    "RetryDecision",
     "WriteType",
 ]
