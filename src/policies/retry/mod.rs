542af0c85e3a87f6d00f0830347eaea5fd433ec1
Paulina Czajkowska <paulina.czajkowska04@gmail.com>
2026-06-02 22:15:13 +0200

    add: `PyRetryDecision`

--- src/policies/retry/mod.rs
+++ src/policies/retry/mod.rs
@@ -1,9 +1,11 @@
 use pyo3::prelude::*;
 
+pub mod decision;
 pub mod types;
 
 #[pymodule]
 pub(crate) fn retry_policy(_py: Python<'_>, module: &Bound<'_, PyModule>) -> PyResult<()> {
+    module.add_class::<decision::PyRetryDecision>()?;
     module.add_class::<types::PyCqlResponseKind>()?;
     module.add_class::<types::PyOperationType>()?;
     module.add_class::<types::PyWriteType>()?;
